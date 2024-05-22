448027472a9e583a0b205ff3cb74d8262af0b0af
Jere1203 <jeremias.bartolsic@gmail.com>
2024-05-22 14:55:49 -0300

    Avance en heroes de leyenda

--- Funcional/Parciales/Heroes_de_leyenda.hs
+++ Funcional/Parciales/Heroes_de_leyenda.hs
@@ -2,7 +2,8 @@ data Heroe = Heroe{
     nombreHeroe :: String,
     epiteto :: String,
     reconocimiento :: Int,
-    artefactos :: [Artefacto]
+    artefactos :: [Artefacto],
+    tareas :: [Tarea]
 } deriving (Show, Eq)
 
 data Artefacto = Artefacto{
@@ -10,17 +11,44 @@ data Artefacto = Artefacto{
     rareza :: Int
 } deriving(Show, Eq)
 
-jorge = (Heroe "Jorge" "" 1001 [])
-luis = (Heroe "Luis" "" 850 [])
-jeremias = (Heroe "Jeremias" "" 300 [])
-pedro = (Heroe "Pedro" "" 50 [])
+jorge :: Heroe
+jorge = Heroe "Jorge" "" 1001 []
 
+luis :: Heroe
+luis = Heroe "Luis" "" 850 []
+
+jeremias :: Heroe
+jeremias = Heroe "Jeremias" "" 300 []
+
+pedro :: Heroe
+pedro = Heroe "Pedro" "" 50 []
+
+lanzaOlimpo :: Artefacto
 lanzaOlimpo = (Artefacto "Lanza del Olimpo" 100)
+xiphos :: Artefacto
 xiphos = (Artefacto "Xiphos" 50)
 
 pasaAHistoria :: Heroe -> Heroe 
-pasaAHistoria (Heroe nombreHeroe epiteto reconocimiento artefactos)
-    |reconocimiento > 1000 = (Heroe nombreHeroe (epiteto ++ "El mitico") reconocimiento artefactos) --No recibe artefacto
-    |reconocimiento >= 500 && reconocimiento <= 1000 = (Heroe nombreHeroe (epiteto ++ "El magnifico") reconocimiento (lanzaOlimpo : artefactos))
-    |reconocimiento < 500 && reconocimiento > 100 = (Heroe nombreHeroe (epiteto ++ "Hoplita") reconocimiento (xiphos : artefactos))
-    |otherwise = (Heroe nombreHeroe epiteto reconocimiento artefactos)
+pasaAHistoria (Heroe nombreHeroe epiteto reconocimiento artefactos tareas)
+    |reconocimiento > 1000 = (Heroe nombreHeroe "El mitico" reconocimiento artefactos tareas) --No recibe artefacto
+    |reconocimiento >= 500 = (Heroe nombreHeroe "El magnifico" reconocimiento (lanzaOlimpo : artefactos) tareas)
+    |reconocimiento >  100 = (Heroe nombreHeroe "Hoplita" reconocimiento (xiphos : artefactos) tareas)
+    |otherwise             = (Heroe nombreHeroe epiteto reconocimiento artefactos tareas)
+
+
+
+-- Tareas --
+
+type Tarea = Heroe -> Heroe
+
+encontrarArtefacto :: Artefacto -> Tarea
+    encontrarArtefacto unArtefacto (Heroe _ _ reconocimiento artefactos _) = (Heroe (aumentarReconocimiento rareza unArtefacto) (unArtefacto : artefactos))
+
+aumentarReconocimiento :: Int -> Heroe -> Heroe
+aumentarReconocimiento cantidad (Heroe _ _ reconocimiento _ _) = reconocimiento + cantidad
+
+escalarElOlimpo :: Tarea
+escalarElOlimpo (Heroe _ _ reconocimiento artefactos _) = (Heroe aumentarReconocimiento 500 triplicarRarezaArtefactos (relampagoDeZeus : artefactos))
+
+triplicarRarezaArtefactos :: Heroe -> Heroe
+triplicarRarezaArtefactos (Heroe _ _ _ artefactos _) = map (*3) rareza.artefactos
